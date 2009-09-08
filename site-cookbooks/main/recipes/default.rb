6cc4364d2dba4fad2b90c15ba532dbb5bb074d7b
Jonathan Goldman <me@jonnii.com>
2009-09-07 21:30:40 -0400

    oops

--- site-cookbooks/main/recipes/default.rb
+++ site-cookbooks/main/recipes/default.rb
@@ -29,7 +29,7 @@ include_recipe "rails"
 include_recipe "passenger_apache2"
 
 web_app "twentythirdandlove" do
-  cookbook "passenger"
+  cookbook "passenger_apache2"
   docroot "/apps/twentythirdandlove/public"
   server_name "#{node[:domain]}"
   server_aliases [ "twentythirdandlove", node[:hostname] ]
